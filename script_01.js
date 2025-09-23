bf9c1de5a417984f9c7e032916e0007fcc71da11
kamala mahara <kmlamahara@gmail.com>
2025-09-23 22:39:14 +0545

    Add initial HTML structure and JavaScript variables and data types

---
+++ script_01.js
@@ -0,0 +1,35 @@
+// js variables  and datatypes
+ let user ="kamla";
+ let  User = "prativa";      //js is a case-sensitive lang
+
+ let $salary=2323423; 
+ let user_Age= 19;             
+ let income=0.00000;
+
+
+ let developer;            // undefined means this hasn’t been assigned yet, maybe it’s a mistake.maybe programmer forgot to assign.
+ let x=null;              // null means like I’ve assigned it, and I want it to be empty.null removes ambiguity.
+
+ let isIntrovert=true;
+ let dancer_hou=false;    
+
+ let z=BigInt("123");
+ let y=Symbol("hello");
+ const student={
+  name: "kamla",
+  age:19,
+  gpa:3.70,
+  isTopper:false,
+ };
+
+ 
+ //keywords
+
+ var a=10;
+ var a=20;
+ var a=30;       // var:-- variable can be re-declared and updated.its global scope
+
+ let b=40;       // let:-- variable cant be re-declared but can updated.its block scope.
+
+ const pi=3.14;   // const:-- variable cant be redeclared and updated.its a block scope.
+ 
