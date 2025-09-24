8eed23e7b1d8201205567d7f825d0aeb9c863cad
kamala mahara <kmlamahara@gmail.com>
2025-09-24 06:55:59 +0545

    Add JavaScript file for operators and conditional statements

---
+++ script_02.js
@@ -0,0 +1,29 @@
+//operators and conditional statements in js
+ // operatoes are used to perform the operation on data
+
+ //Arithmetic operators  (they need at least 2 operands to perform operation)
+
+ let a=100;
+ let b=120;
+
+ console.log("a = ",a +" b =",b);
+ console.log("a + b=",a + b);      //addition
+ console.log("a - b =",a-b);  //sub
+ console.log("a * b =",a*b); //mul
+ console.log("a / b =",a/b); //div
+ console.log("a % b =",a%b);  //mod
+ console.log("a ** b=",a**b); //exponential
+
+
+ //unary operator (they need only one operand
+ //post inr/dcr
+ console.log("a++ =",a++); //a=100
+ console.log("a++ =",a);   //a=101      // a++ is a post increment  operator, it will first print the value then increses its value by 1
+
+ console.log("a-- =",a--);  
+ console.log("a-- =",a);       //a-- is a post decrement operator, it will first print the value of the operand then decreases by 1
+
+ //pre inr/dcr
+ console.log("++b =",++b);
+ console.log("--b =",--b);    // ++b & --b are pre inr/dcr operator,they will first increse/decrease the value then print.
+
