0f8e773fcd6021598f537c2a4ec25da871943c32
kamala mahara <kmlamahara@gmail.com>
2025-10-01 23:37:18 +0545

    Add function examples for area calculation and multiplication using arrow functions

--- script_05.js
+++ script_05.js
@@ -1,1 +1,19 @@
 //js functions
+//functions are the block of resuable code designed to p erform a specific task 
+function calculate(l,b){   //function definition with parameters
+  let area=l*b;
+  return area;
+}
+let area=calculate(20,40);  //function calling with arguments
+console.log("the area of rectangle is:",area);
+
+
+//Arrow function (modern js)
+//its a shorthand syntax for writing functions introduced in ES6,they allow us to create more concise and readable code.
+let arrowfun=(a,b)=>{
+  return a*b;
+}
+
+let mul= arrowfun(30,40);
+console.log("the multiply is:",mul);
+
