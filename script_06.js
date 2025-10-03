5f3e2f078823a98fecc31cf00a2b79c8ccaa6720
kamala mahara <kmlamahara@gmail.com>
2025-10-03 23:38:09 +0545

    Enhance script_06.js with additional DOM manipulation examples and improve comments for clarity

--- script_06.js
+++ script_06.js
@@ -1,4 +1,4 @@
-//DOM METHODS
+// // //DOM METHODS
 //getElementById()
 let element=document.getElementById("id"); //returns the element with the specified id
 console.log(element);
@@ -21,4 +21,20 @@ console.log(firstElement); //it returns only one  first element that matches the
 let allelements=document.querySelectorAll(".class"); //returns a static nodelist of all elements that matches a specidied css selector
 console.log(allelements);//it returns all the elements that matches the selctor .class 
 
+//tagname
+let a=document.querySelector(".class");
+console.log(a);
 
+//InnerText
+console.log(a.innerText);//it returns the plain text contained within the element
+
+let text=document.querySelector("#id")
+text.innerText="its the text element changed by inner text property in the id div"; // it sets the content of the element 
+console.log(text.innerText);
+//innerHTML
+console.log(a.innerHTML);//it returns the html content of the element
+a.innerHTML="<h2>good night guys literally its 11:30 pm</h2>";//it sets the html content of the element
+console.log(a.innerHTML);
+
+//textContent
+console.log(a.textContent);//it returns the text content of the element including hidden elements
